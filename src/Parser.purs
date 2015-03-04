a4dfd005e402d19fe8ff7142d693b5c404df0b75
Stefan Kögel <stefan.koegel@uni-ulm.de>
2015-03-04 14:26:09 +0100

    Add operator sections to the parser

--- src/Parser.purs
+++ src/Parser.purs
@@ -3,6 +3,7 @@ module Parser where
 import Global              (readInt)
 import Data.String         (joinWith, split)
 import Data.Identity       (Identity())
+import Data.Maybe
 import Control.Alt         ((<|>))
 import Control.Apply       ((*>))
 import Control.Alternative (some, many)
@@ -70,39 +71,66 @@ bracket expr = do
   string ")"
   return e
 
+section :: Parser String Expr -> Parser String Expr
+section expr = do
+  mop1 <- optionMaybe $ choice ops
+  eatSpaces
+  e <- optionMaybe $ base expr
+  eatSpaces
+  mop2 <- optionMaybe $ choice ops
+  match mop1 e mop2
+  where
+  match (Just op) (Just e)  (Nothing) = return $ SectR op e
+  match (Nothing) (Just e)  (Just op) = return $ SectL e op
+  match (Nothing) (Just e)  (Nothing) = return e
+  match (Just op) (Nothing) (Nothing) = return $ (App (show op) [])
+  match _         _         _         = fail "Neither section nor expression!"
+  ops =
+    [ op (string "+" *> notFollowedBy (string "+")) Add
+    , op (string "-" *> notFollowedBy num) Sub
+    , op (string "*")     Mul
+    , op (string "`div`") Div
+    , op (string ":")     Cons
+    , op (string "++")    Append
+    , op (string "&&")    And
+    , op (string "||")    Or
+    ]
+
 base :: Parser String Expr -> Parser String Expr
-base expr = list expr <|> bracket expr <|> (Atom <$> atom)
+base expr =  list expr
+         <|> (bracket $ section expr)
+         <|> (Atom <$> atom)
 
 termApp :: Parser String Expr -> Parser String Expr
 termApp expr = try (app (base expr)) <|> base expr
 
-op :: forall a. Parser String a -> Op -> Parser String (Expr -> Expr -> Expr)
-op opParser opType = try $
-  (eatSpaces *> opParser *> eatSpaces) *> return (Binary opType)
+op :: forall a. Parser String a -> Op -> Parser String Op
+op opParser opConstructor = try $
+  (eatSpaces *> opParser *> eatSpaces *> return opConstructor)
 
 term7l :: Parser String Expr -> Parser String Expr
 term7l expr = chainl1 (termApp expr) (mulP <|> divP)
   where
-  mulP = op (string "*") Mul
-  divP = op (string "`div`") Div
+  mulP = Binary <$> op (string "*") Mul
+  divP = Binary <$> op (string "`div`") Div
 
 term6l :: Parser String Expr -> Parser String Expr
 term6l expr = chainl1 (term7l expr) (addP <|> subP)
   where
-  addP = op (string "+" *> notFollowedBy (string "+")) Add
-  subP = op (string "-") Sub
+  addP = Binary <$> op (string "+" *> notFollowedBy (string "+")) Add
+  subP = Binary <$> op (string "-") Sub
 
 term5r :: Parser String Expr -> Parser String Expr
 term5r expr = chainr1 (term6l expr) (consP <|> appendP)
   where
-  consP = op (string ":") Cons
-  appendP = op (string "++") Append
+  consP = Binary <$> op (string ":") Cons
+  appendP = Binary <$> op (string "++") Append
 
 term3r :: Parser String Expr -> Parser String Expr
-term3r expr = chainr1 (term5r expr) (op (string "&&") And)
+term3r expr = chainr1 (term5r expr) (Binary <$> op (string "&&") And)
 
 term2r :: Parser String Expr -> Parser String Expr
-term2r expr = chainr1 (term3r expr) (op (string "||") Or)
+term2r expr = chainr1 (term3r expr) (Binary <$> op (string "||") Or)
 
 expr :: Parser String Expr
 expr = fix1 $ \expr -> term2r expr
