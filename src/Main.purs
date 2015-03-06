a67bedc4e5a0b325900ab0bcbdd75a637ac0c940
Stefan Kögel <stefan.koegel@uni-ulm.de>
2015-03-06 14:32:12 +0100

    Add code to interactively test the web frontend

--- src/Main.purs
+++ src/Main.purs
@@ -2,8 +2,29 @@ module Main where
 
 import qualified Control.Monad.JQuery as J
 import           Control.Monad.Eff
+import DOM
+
+import Data.Either
+
+import Web
+import Parser
+import Evaluator
+import AST
 
 import Debug.Trace
 
+
 main = J.ready $ do
   print "Hello world!"
+  let expr = case parseExpr "1 + 2 * 3 + 4 * 5 * 6" of Right e -> e
+  let env = defsToEnv $ case parseDefs "double x = x + x" of Right d -> d
+  showExpr expr
+  return unit
+
+showExpr :: forall eff. Expr -> Eff (dom :: DOM | eff) Unit
+showExpr expr = do
+  test <- J.select "#test"
+  J.clear test
+  jexpr <- exprToJQuery expr showExpr
+  J.append jexpr test
+  return unit
