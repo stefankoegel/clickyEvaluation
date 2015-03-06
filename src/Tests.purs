30f385eb0b111a429a3445443513e08f0bd5e2ba
Stefan Kögel <stefan.koegel@uni-ulm.de>
2015-03-06 10:37:18 +0100

    Add module for simpler testing in 'psci' (repl)

---
+++ src/Tests.purs
@@ -0,0 +1,22 @@
+module Tests where
+
+import Data.Either
+
+import AST
+import Parser
+import Evaluator
+
+
+testEnv :: Env
+testEnv = defsToEnv $ case (Parser.parseDefs defs) of Right env -> env
+  where
+  defs = """
+    double x = x + x
+    fact 0 = 1
+    fact n = n * fact (n - 1)
+    fib 0 a b = a
+    fib n a b = fib (n - 1) b (a + b)
+    """
+
+testExpr :: String -> Expr
+testExpr str = case (Parser.parseExpr str) of Right expr -> expr
