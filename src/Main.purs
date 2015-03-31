00d42074bbd8d6871e87c5997643ea602a8454f5
Stefan Kögel <stefan.koegel@uni-ulm.de>
2015-03-31 12:48:07 +0200

    Enable undoing and deleting of elements in history

--- src/Main.purs
+++ src/Main.purs
@@ -7,7 +7,8 @@ import Data.Foreign (unsafeFromForeign)
 
 import Data.Either
 import Data.Maybe
-import Data.Foldable (for_)
+import Data.Array ((..), length, deleteAt, (!!), drop)
+import Data.Traversable (zipWithA)
 import Control.Apply ((*>))
 import Control.Monad.State.Trans
 import Control.Monad.State.Class
@@ -61,7 +62,7 @@ showEvaluationState = do
   { env = env, expr = expr, history = histExprs } <- get :: EvalM EvalState
 
   liftEff $ exprToJQuery expr >>= wrapInDiv "output" >>= flip J.append output
-  liftEff $ showHistory histExprs >>= flip J.append history
+  showHistoryList histExprs >>= liftEff <<< flip J.append history
 
   liftEff (J.find ".binary, .app, .func" output)
     >>= makeClickable
@@ -72,12 +73,39 @@ showEvaluationState = do
 
   liftEff $ return unit :: DOMEff Unit
 
-showHistory :: [Expr] -> DOMEff J.JQuery
-showHistory exprs = do
-  box <- J.create "<div></div>" >>= J.addClass "historyBox"
-  for_ exprs $ \expr -> do
-    exprToJQuery expr >>= wrapInDiv "history" >>= wrapInDiv "vertical" >>= flip J.append box
+forIndex :: forall m a b. (Applicative m) => [a] -> (a -> Number -> m b) -> m [b]
+forIndex as f = zipWithA f as (0 .. (length as - 1))
+
+showHistoryList :: [Expr] -> EvalM J.JQuery
+showHistoryList exprs = do
+  box <- liftEff $ J.create "<div></div>" >>= J.addClass "historyBox"
+  forIndex exprs $ \expr i -> do
+    showHistory expr i >>= liftEff <<< wrapInDiv "vertical" >>= liftEff <<< flip J.append box
   return box
+
+(!!!) :: forall a. [a] -> Number -> a
+(!!!) xs i = case xs !! i of Just x -> x
+
+showHistory :: Expr -> Number -> EvalM J.JQuery
+showHistory expr i = do
+  history <- liftEff $ J.create "<div></div>" >>= J.addClass "history"
+  liftEff $ exprToJQuery expr >>= flip J.append history
+  es <- get :: EvalM EvalState
+  let deleteHandler = \_ _ -> do
+                        let es' = es { history = deleteAt i 1 es.history }
+                        void $ runStateT showEvaluationState es'
+  delete <- liftEff $ J.create "<button></button>"
+    >>= J.setText "Delete"
+    >>= J.on "click" deleteHandler
+  liftEff $ J.append delete history
+  let restoreHandler = \_ _ -> do
+                         let es' = es { history = drop (i + 1) es.history, expr = es.history !!! i }
+                         void $ runStateT showEvaluationState es'
+  restore <- liftEff $ J.create "<button></button>"
+    >>= J.setText "Restore"
+    >>= J.on "click" restoreHandler
+  liftEff $ J.append restore history
+  return history
 
 prepareContainer :: String -> DOMEff J.JQuery
 prepareContainer name = do
