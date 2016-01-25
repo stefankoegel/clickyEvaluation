213d4d4a376c073095642a68517d5cbe7c24df3c
Joscha Cueppers <joscha.cueppers@uni-ulm.de>
2016-01-25 22:49:31 +0100

    Added foreign import of functions from jquery and raphael

--- src/JSHelpers.js
+++ src/JSHelpers.js
@@ -18,12 +18,44 @@ exports.isEnterKey = function(event) {
 // creates Canvas and return
 // canvas already in html side
 exports.createCanvas = function(){
-  var svgContainer = document.getElementById("svg-container");
-  while (svgContainer.hasChildNodes()) {
-    svgContainer.removeChild(svgContainer.lastChild);
-  }
-  var outContainer = document.getElementById("topLevelOutput-container");
-  var canvas = Raphael(svgContainer,$(outContainer).width(),$(outContainer).height());
-  svgContainer.style.top = $(outContainer).position().top + "px"
-  return canvas
-}
+    var svgContainer = document.getElementById("svg-container");
+    while (svgContainer.hasChildNodes()) {
+      svgContainer.removeChild(svgContainer.lastChild);
+    }
+    var outContainer = document.getElementById("topLevelOutput-container");
+    var canvas = Raphael(svgContainer,$(outContainer).width(),$(outContainer).height());
+    svgContainer.style.top = $(outContainer).position().top + "px";
+    return canvas;
+};
+
+exports.offsetTop = function(ob){
+  return function(){
+    return $(ob).offset().top;
+  };
+};
+
+exports.offsetLeft = function(ob){
+  return function(){
+    return $(ob).offset().left;
+  };
+};
+
+exports.outerHeight = function(ob){
+  return function(){
+    return $(ob).outerHeight();
+  };
+};
+
+exports.outerWidth = function(ob){
+  return function(){
+    return $(ob).outerWidth();
+  };
+};
+
+exports.path = function(canvas) {
+  return function(str){
+    return function (){
+      canvas.path(str);
+    };
+  };
+};
--- src/JSHelpers.purs
+++ src/JSHelpers.purs
@@ -11,3 +11,18 @@ foreign import jqMap :: forall eff. (JQuery -> Eff (dom :: DOM | eff) Unit) -> J
 foreign import isEnterKey :: JQueryEvent -> Boolean
 
 foreign import createCanvas :: forall eff. Eff (dom :: DOM | eff) Foreign
+
+
+ -- extension of  JQuery interface
+foreign import offsetTop :: forall eff. JQuery -> Eff (dom :: DOM | eff) Number
+
+foreign import offsetLeft :: forall eff. JQuery -> Eff (dom :: DOM | eff) Number
+
+foreign import outerHeight :: forall eff. JQuery -> Eff (dom :: DOM | eff) Number
+
+foreign import outerWidth :: forall eff. JQuery -> Eff (dom :: DOM | eff) Number
+
+-- interface to Raphael libary
+-- Foreign is Canvas from createCanvas
+-- 2nd is SVG path
+foreign import path:: forall eff. Foreign -> String -> Eff (dom :: DOM | eff ) Unit
